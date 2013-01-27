07903898324726123d09415cd819a4bb89aaa4a6
Ryan Alyn Porter <rap@endymion.com>
2013-01-27 15:30:59 -0500

    Initial commit.

---
+++ leaderboard.js
@@ -0,0 +1,27 @@
+var banners = [
+  {
+    "img": "tnl-banner-steve-jobs-01.gif",
+    "url": "http://news.stanford.edu/news/2005/june15/jobs-061505.html",
+    "weight": 4
+  },
+  {
+    "img": "tnl-banner-steve-jobs-02.gif",
+    "url": "http://news.stanford.edu/news/2005/june15/jobs-061505.html",
+    "weight": 1
+  },
+  {
+    "img": "tnl-banner-steve-jobs-03.gif",
+    "url": "http://news.stanford.edu/news/2005/june15/jobs-061505.html",
+    "weight": 1
+  },
+  {
+    "img": "tnl-banner-steve-jobs-04.gif",
+    "url": "http://news.stanford.edu/news/2005/june15/jobs-061505.html",
+    "weight": 1
+  },
+  {
+    "img": "tnl-banner-steve-jobs-05.gif",
+    "url": "http://news.stanford.edu/news/2005/june15/jobs-061505.html",
+    "weight": 1
+  }
+];
